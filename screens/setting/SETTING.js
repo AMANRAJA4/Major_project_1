e28df137d7992172a51d490e85330d190b7f76c9
Aman Raja <aman1672003@gmail.com>
2023-05-31 17:26:39 +0530

    7

---
+++ screens/setting/SETTING.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SETTING = () => {
+  return (
+    <View>
+      <Text>SETTING</Text>
+    </View>
+  )
+}
+
+export default SETTING
+
+const styles = StyleSheet.create({})
